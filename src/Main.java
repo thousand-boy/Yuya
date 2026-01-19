3e55d24eb423d02f3fa4983909b1fa3567b35d0e
thousand-boy <work.inside1210@gmail.com>
2026-01-19 17:29:10 +0900

    day2: add if-else for change/shortage

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
 
         int price = 680;
-        int paid = 1000;
-        int change = paid - price;
+        int paid = 680;   // ← まず不足の例
 
         System.out.println("商品金額: " + price + "円");
         System.out.println("支払額: " + paid + "円");
-        System.out.println("お釣り: " + change + "円");
+
+        if (paid >= price) {
+            int change = paid - price;
+            System.out.println("お釣り: " + change + "円");
+        } else {
+            int shortage = price - paid;
+            System.out.println("あと " + shortage + "円 足りません");
+        }
     }
 }
